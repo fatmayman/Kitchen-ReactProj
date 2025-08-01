fa66ce9fd501a4a772c5c29eaafab01a09597125
fatmayman <1fatmayman@gmail.com>
2025-08-01 21:42:43 +0300

    fix router duplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import MealDetail from './pages/MealDetail.jsx'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meal/:id" element={<MealDetail />} />
       </Routes>
-      </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
